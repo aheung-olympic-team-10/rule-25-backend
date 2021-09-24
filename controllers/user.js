f720fdf825fb1d521af0d5c9edfdecc1dcd8ada3
devHudi <devhudi@gmail.com>
2021-09-24 18:29:48 +0900

    feat: implement user read api

--- controllers/user.js
+++ controllers/user.js
@@ -1,140 +1,22 @@
 const db = require('../models');
-const User = db.users;
+const User = db.user;
+const Follow = db.follow;
+const Notification = db.notification;
 const Op = db.Sequelize.Op;
 
-// Create and save a new user
-exports.create = (req, res) => {
-  if (!req.body.title) {
-    res.status(400).send({
-      message: 'Content can not be empty!',
-    });
-    return;
-  }
+exports.findOne = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, description } = await User.findOne({ where: { id } });
 
-  const user = {
-    title: req.body.title,
-    description: req.body.description,
-    published: req.body.published ? req.body.published : false,
-  };
+  const followerCount = await Follow.count({
+    where: { followingId: id },
+  }); // 해당 유저의 팔로워
 
-  User.create(user)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || 'Some error occurred while creating the user.',
-      });
-    });
-};
+  const followingCount = await Follow.count({
+    where: { followerId: id },
+  }); // 해당 유저가 팔로우 중인 사람
 
-// Retrieve all Users from the database.
-exports.findAll = (req, res) => {
-  const title = req.query.title;
-  var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
-
-  User.findAll({ where: condition })
-    .then((data) => {
-      console.log(data);
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || 'Some error occurred while retrieving users.',
-      });
-    });
-};
-
-// Find a single User with an id
-exports.findOne = (req, res) => {
-  const id = req.params.id;
-
-  User.findByPk(id)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: 'error retrieving User with id =' + id,
-      });
-    });
-};
-
-// Update a User by the id in the request
-exports.update = (req, res) => {
-  const id = req.params.id;
-
-  User.update(req.body, {
-    where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: 'User was updated successfully.',
-        });
-      } else {
-        res.send({
-          message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: 'Error updating User with id= ' + id,
-      });
-    });
-};
-
-// Delete a User with the specified id in the request
-exports.delete = (req, res) => {
-  const id = req.params.id;
-
-  User.destroy({
-    where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: 'User was deleted successfully!',
-        });
-      } else {
-        res.send({
-          message: `Cannot delete User with id =${id}. Maybe User was not found!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: 'Could not delete User with id = ' + id,
-      });
-    });
-};
-
-// Delete all Users from the database.
-exports.deleteAll = (req, res) => {
-  User.destroy({
-    where: {},
-    truncate: false,
-  })
-    .then((nums) => {
-      res.send({ message: `${nums} Users were deleted successfully!` });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || 'Some error occurred while removingall users.',
-      });
-    });
-};
-
-// Find all published Users
-exports.findAllPublished = (req, res) => {
-  User.findAll({ where: { published: true } })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || 'Some error occurred while retrieving users.',
-      });
-    });
+  res
+    .status(200)
+    .json({ name, email, description, followerCount, followingCount });
 };
